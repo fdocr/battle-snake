af48f41849844f71b7a46c0531ab9d93a5527f37
Fernando Valverde <fdov88@gmail.com>
2020-12-16 08:57:31 -0600

    Personalization

--- app/app.rb
+++ app/app.rb
@@ -10,11 +10,11 @@ use Rack::PostBodyContentTypeParser
 # TIP: If you open your Battlesnake URL in browser you should see this data
 get '/' do
   appearance = {
-    apiversion: "1",        
-    author: "",           # TODO: Your Battlesnake Username
-    color: "#888888",     # TODO: Personalize
-    head: "default",      # TODO: Personalize
-    tail: "default",      # TODO: Personalize
+    apiversion: "1",
+    author: "Pinky.py",           # TODO: Your Battlesnake Username
+    color: "#DA00CB",     # TODO: Personalize
+    head: "silly",      # TODO: Personalize
+    tail: "bolt",      # TODO: Personalize
   }
 
   camelcase(appearance).to_json
